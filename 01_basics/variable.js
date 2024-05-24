cae26b04544299038ab503bc8cf908d8dd26dd06
Bhedpraksh <127827568+Codebsbs@users.noreply.github.com>
2024-05-24 00:57:11 +0000

    variable type

---
+++ 01_basics/variable.js
@@ -0,0 +1,7 @@
+const accountId=123;
+let accountName="staya"
+var accountPassWord="765"
+accountMail="s@gmail.com"
+let accountType;
+console.log(accountMail)
+console.table([accountId,accountName,accountPassWord,accountMail,accountType]);
